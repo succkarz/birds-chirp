13860acbf6eb3d4c5630bf897f506da9f98821fa
succkarz <succkarz@gmail.com>
2017-04-30 13:04:04 +0530

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,37 @@
-$(window).scroll(function(){
-    var wScroll = $(this).scrollTop();
-    $('.logo').css({
-      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-    });
-    $('.back-bird').css({
-      'transform' : 'translate(0px, '+ wScroll /5 +'%)'
-    });
-    $('.fore-bird').css({
-      'transform' : 'translate(0px, -'+ wScroll /10 +'%)'
-    });
-    if (wScroll > $('.clothes-pics').offset().top - $(window).height() / 1.5) {
+$(window).scroll(function() {
+  var wScroll = $(this).scrollTop();
+  $('.logo').css({
+    'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+  });
+  $('.back-bird').css({
+    'transform': 'translate(0px, ' + wScroll / 5 + '%)'
+  });
+  $('.fore-bird').css({
+    'transform': 'translate(0px, -' + wScroll / 10 + '%)'
+  });
+  if (wScroll > $('.clothes-pics').offset().top - $(window).height() / 1.5) {
 
-            $('.clothes-pics figure').each(function(i) {
-              setTimeout(function(){
+    $('.clothes-pics figure').each(function(i) {
+      setTimeout(function() {
 
-                $('.clothes-pics figure').eq(i).addClass('is-showing');
-              }, 150 * (i+1));
-            });
-        }
+        $('.clothes-pics figure').eq(i).addClass('is-showing');
+      }, 150 * (i + 1));
+    });
+  } 
+  /*
+    if(wScroll > $('.blog-posts').offset().top - $(window).height()){
+	  var offset = Math.min(0, wScroll - 
+	  $('.blog-posts').offset().top + $(window).height() - 350);
+	  $('.post-1').css({'transform': 'translate('+ offset +'px, 20px)'});
+	  $('.post-3').css({'transform': 'translate('+ offset +'px, 20px)'});
+  } */
+ if(wScroll > $('.blog-posts').offset().top - $(window).height()){
+	  var offset = Math.min(0, wScroll - 
+	  $('.blog-posts').offset().top + $(window).height() - 400);
+	  $('.post-1').css({'transform': 'translate('+ offset +'px, '+ 
+	  Math.abs(offset * 0.2) +'px)'});
+	  $('.post-3').css({'transform': 'translate('+ 
+	  Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
+  }
+
 });
